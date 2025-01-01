c5d50325b25e9fc4b514ea22b6f6db0cf45f0b87
aish21 <aish.akshu@gmail.com>
2025-01-01 14:11:09 +0800

    Add exp component

---
+++ revamped-personal-website/src/App.tsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Main,
+  Experience,
+  SkillsStack,
+  ProjectsComponent,
+  ContactMe,
+  NavBar,
+  PageFooter,
+} from './components';
+import AnimateComponent from './components/AnimateComponent';
+import './index.scss';
+
+function App() {
+  const [mode, setMode] = useState<string>('dark');
+
+  const handleModeChange = () => {
+    if (mode === 'dark') {
+      setMode('light');
+    } else {
+      setMode('dark');
+    }
+  };
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }, []);
+
+  return (
+    <div
+      className={`main-container ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}
+    >
+      <NavBar parentToChild={{ mode }} modeChange={handleModeChange} />
+      <AnimateComponent transitionDuration={700}>
+        <Main />
+        <SkillsStack />
+        <Experience />
+        <ProjectsComponent />
+        <ContactMe />
+      </AnimateComponent>
+      <PageFooter />
+    </div>
+  );
+}
+
+export default App;
---
+++ revamped-personal-website/src/components/index.js
@@ -0,0 +1,7 @@
+export { default as NavBar } from './NavBar';
+export { default as PageFooter } from './PageFooter';
+export { default as Main } from './Main';
+export { default as SkillsStack } from './SkillsStack';
+export { default as Experience } from './Experience';
+export { default as ProjectsComponent } from './ProjectsComponent';
+export { default as ContactMe } from './ContactMe';
